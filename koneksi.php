35da724dcb16e5834ba46861b676e02146e8bcdf
Anisa Muziya Rafa <amrxll08@gmail.com>
2025-11-17 00:06:16 +0800

    Add database connection script in koneksi.php

---
+++ koneksi.php
@@ -0,0 +1,8 @@
+<?php
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "pemesanan_hotel";
+$con =  mysqli_connect($server, $username, $password) or die("Koneksi gagal");
+mysqli_select_db($con, $database) or die("Database tidak bisa dibuka");
+?>
