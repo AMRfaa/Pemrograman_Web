de6248d614eff00fc71721e9207cd0b807c3a1bc
Anisa Muziya Rafa <amrxll08@gmail.com>
2025-11-17 00:17:36 +0800

    Update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -1,7 +1,8 @@
-CREATE DATABASE IF NOT EXISTS pemesanan_hotel;
-USE pemesanan_hotel;
+-- DATABASE
+CREATE DATABASE IF NOT EXISTS db_pemesanan_hotel;
+USE db_pemesanan_hotel;
 
--- Tabel pengguna (admin / petugas / tamu)
+-- TABEL PENGGUNA (admin / petugas / tamu)
 CREATE TABLE IF NOT EXISTS pengguna (
   id_pengguna INT AUTO_INCREMENT PRIMARY KEY,
   nama_pengguna VARCHAR(50) NOT NULL UNIQUE,
@@ -20,10 +21,10 @@ CREATE TABLE IF NOT EXISTS kamar (
   status ENUM('tersedia','terisi','perbaikan') DEFAULT 'tersedia'
 );
 
--- Tabel pemesanan
+-- TABEL PEMESANAN
 CREATE TABLE IF NOT EXISTS pemesanan (
   id_pemesanan INT AUTO_INCREMENT PRIMARY KEY,
-  id_pengguna INT NOT NULL,    -- user yang membuat pemesanan (bisa admin/petugas/tamu)
+  id_pengguna INT NOT NULL,
   id_kamar INT NOT NULL,
   nama_tamu VARCHAR(100) NOT NULL,
   tanggal_masuk DATE NOT NULL,
@@ -31,11 +32,11 @@ CREATE TABLE IF NOT EXISTS pemesanan (
   total_harga DECIMAL(14,2) NOT NULL,
   status ENUM('dipesan','checkin','batal','selesai') DEFAULT 'dipesan',
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (id_pengguna) REFERENCES pengguna(id_pengguna) ON DELETE RESTRICT ON UPDATE CASCADE,
-  FOREIGN KEY (id_kamar) REFERENCES kamar(id_kamar) ON DELETE RESTRICT ON UPDATE CASCADE
+  FOREIGN KEY (id_pengguna) REFERENCES pengguna(id_pengguna),
+  FOREIGN KEY (id_kamar) REFERENCES kamar(id_kamar)
 );
 
--- Tabel pembayaran (opsional sederhana)
+-- OPSIONAL: TABEL PEMBAYARAN
 CREATE TABLE IF NOT EXISTS pembayaran (
   id_pembayaran INT AUTO_INCREMENT PRIMARY KEY,
   id_pemesanan INT NOT NULL,
@@ -43,5 +44,5 @@ CREATE TABLE IF NOT EXISTS pembayaran (
   metode VARCHAR(50),
   tanggal_bayar DATETIME DEFAULT CURRENT_TIMESTAMP,
   status ENUM('tertunda','lunas','gagal') DEFAULT 'tertunda',
-  FOREIGN KEY (id_pemesanan) REFERENCES pemesanan(id_pemesanan) ON DELETE CASCADE ON UPDATE CASCADE
+  FOREIGN KEY (id_pemesanan) REFERENCES pemesanan(id_pemesanan)
 );
