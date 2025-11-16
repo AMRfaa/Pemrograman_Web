9b03bc07e174a092f673159b9131905e955efca7
Anisa Muziya Rafa <amrxll08@gmail.com>
2025-11-17 00:15:06 +0800

    Update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -10,22 +10,14 @@ CREATE TABLE IF NOT EXISTS pengguna (
   peran ENUM('admin','petugas','tamu') DEFAULT 'tamu'
 );
 
--- Tabel jenis_kamar
-CREATE TABLE IF NOT EXISTS jenis_kamar (
-  id_jenis INT AUTO_INCREMENT PRIMARY KEY,
-  nama_jenis VARCHAR(50) NOT NULL,
-  kapasitas INT NOT NULL,
-  harga_per_malam DECIMAL(12,2) NOT NULL,
-  deskripsi TEXT
-);
-
--- Tabel kamar
+-- TABEL KAMAR (gabungan kamar + jenis_kamar)
 CREATE TABLE IF NOT EXISTS kamar (
   id_kamar INT AUTO_INCREMENT PRIMARY KEY,
   nomor_kamar VARCHAR(20) NOT NULL UNIQUE,
-  id_jenis INT NOT NULL,
-  status ENUM('tersedia','terisi','perbaikan') DEFAULT 'tersedia',
-  FOREIGN KEY (id_jenis) REFERENCES jenis_kamar(id_jenis) ON DELETE RESTRICT ON UPDATE CASCADE
+  jenis_kamar VARCHAR(50) NOT NULL,       -- contoh: Standar, Deluxe, Suite
+  kapasitas INT NOT NULL,
+  harga_per_malam DECIMAL(12,2) NOT NULL,
+  status ENUM('tersedia','terisi','perbaikan') DEFAULT 'tersedia'
 );
 
 -- Tabel pemesanan
