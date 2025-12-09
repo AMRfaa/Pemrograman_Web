9cf9651d12588966419635cf2319ac852e56ebd0
Anisa Muziya Rafa <amrxll08@gmail.com>
2025-12-09 08:42:37 +0800

    Delete kamar.php

--- kamar.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION['peran']) || $_SESSION['peran'] != 'admin') {
-    echo "Akses ditolak. Hanya admin yang boleh membuka halaman ini.";
-    exit;
-}
-?>
-
